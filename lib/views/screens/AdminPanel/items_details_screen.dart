8f3af4e5ddcbcf5df17949a6c532706db4175e29
azix khan <azixkhan.55@gmail.com>
2024-03-01 11:46:33 +0500

    delete fuctionality

--- lib/views/screens/AdminPanel/items_details_screen.dart
+++ lib/views/screens/AdminPanel/items_details_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-class ItemDetailsScreen extends StatelessWidget {
+class ItemDetailsScreen extends StatefulWidget {
   final String categoryName;
   final String itemId;
 
-  ItemDetailsScreen(
-      {super.key, required this.categoryName, required this.itemId});
+  const ItemDetailsScreen(
+      {Key? key, required this.categoryName, required this.itemId})
+      : super(key: key);
 
+  @override
+  _ItemDetailsScreenState createState() => _ItemDetailsScreenState();
+}
+
+class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
   final CollectionReference _itemsReference =
       FirebaseFirestore.instance.collection('categories');
+
+  late String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +28,9 @@ class ItemDetailsScreen extends StatelessWidget {
       ),
       body: StreamBuilder<DocumentSnapshot>(
         stream: _itemsReference
-            .doc(categoryName)
+            .doc(widget.categoryName)
             .collection('items')
-            .doc(itemId)
+            .doc(widget.itemId)
             .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
@@ -34,15 +43,26 @@ class ItemDetailsScreen extends StatelessWidget {
           var itemDescription = itemData['description'] as String? ?? '';
           var itemImage = itemData['image'] as String? ?? '';
 
+          imageUrl = itemImage;
+
           return Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
                 height: 200,
-                child: Image.network(
-                  itemImage,
-                  fit: BoxFit.cover,
-                ),
+                child: itemImage.isNotEmpty
+                    ? Image.network(
+                        itemImage,
+                        fit: BoxFit.cover,
+                      )
+                    : const Placeholder(
+                        color: Colors.green,
+                        child: Icon(
+                          Icons.delete_forever,
+                          color: Colors.red,
+                          size: 70,
+                        ),
+                      ), // You can replace Placeholder() with any other widget or message
               ),
               Container(
                 padding: const EdgeInsets.all(16.0),
@@ -86,13 +106,9 @@ class ItemDetailsScreen extends StatelessWidget {
                     child: const Text('Update Item'),
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      // Implement delete item functionality
-                      // Show a confirmation dialog before deleting
-                    },
+                    onPressed: _deleteItem,
                     style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          Colors.red, // Change the button color to red
+                      backgroundColor: Colors.red,
                     ),
                     child: const Text('Delete Item'),
                   ),
@@ -103,5 +119,49 @@ class ItemDetailsScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  // delete item functionality
+
+  Future<void> _deleteItem() async {
+    bool confirmDelete = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this item?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmDelete != null && confirmDelete) {
+      await _itemsReference
+          .doc(widget.categoryName)
+          .collection('items')
+          .doc(widget.itemId)
+          .delete();
+
+      if (imageUrl.isNotEmpty) {
+        Reference referenceToDelete =
+            FirebaseStorage.instance.refFromURL(imageUrl);
+        await referenceToDelete.delete();
+      }
+
+      Navigator.pop(context);
+    }
   }
 }
